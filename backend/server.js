8a721f0e0568dbb9ee36ee8c0d026ee08d5e479d
Aman Kashyap <amankashyap0246@gmail.com>
2023-06-09 09:09:59 +0530

    d5

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONCOCONNECT).then(()=>{console.log("mongoDb Connec
 app.use(a)
 app.use(cors({
   credentials:true,
-origin:"https://ai-beige-chi.vercel.app"}
+origin:"https://ai-aman0246.vercel.app"}
 ))
 
 //=================================
